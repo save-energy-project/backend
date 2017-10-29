eaa7bc37cf764e9e61f3f4273a56e66abfcce5a3
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2017-10-29 10:04:30 -0400

    Add test user

--- src/server.js
+++ src/server.js
@@ -11,9 +11,10 @@ const lcToUsd = 54.83;
 app.post('/api/donate', function (request, result) {
     const projectId = request.query['project_id'];
     const amount = request.query['amount'];
+    const user = request.query['user'];
     const btc = amount / btcToUsd;
-    if (projectId && amount) {
-        donate(btc, projectId, result);
+    if (projectId && amount && user) {
+        donate(btc, projectId, user, result);
     }
 });
 
@@ -34,6 +35,43 @@ app.post('/api/create_user', function (request, result) {
 
 app.get('/api/get_balance', function (request, result) {
     coinbase.getBalance(result);
+});
+
+app.get('/api/get_test_user', function (request, result) {
+    const user = '-KxcBQXpMt7RL_GjciJW';
+    databaseRef.child(`users/${user}`)
+    .once('value')
+    .then(function(snapshot) {
+        if (snapshot.val()) {
+            const testUser = snapshot.val();
+            result.status(200).send(testUser);
+        } else {
+            result.sendStatus(500);
+        }
+    });
+});
+
+app.get('/api/get_ownership', function (request, result) {
+    const projectId = request.query['project_id'];
+    const user = request.query['user'];
+    if (!projectId || !user) {
+        result.send(400);
+        return;
+    }
+
+    databaseRef.child(`projects/${projectId}/pledges/${user}`)
+    .once('value')
+    .then(function(snapshot) {
+        if (snapshot.val()) {
+            result.status(200).send({
+                "ownership" : snapshot.val()['amount']
+            });
+        } else {
+            result.status(200).send({
+                "ownership" : 0
+            });
+        }
+    });
 });
 
 app.get('/api/get_projects', function (request, result) {
@@ -65,7 +103,9 @@ app.get('/api/get_project', function (request, result) {
 
     databaseRef.child(`projects/${projectId}`).once('value').then(function(snapshot) {
         if (snapshot.val()) {
-            result.status(200).send(snapshot.val());
+            var project = snapshot.val();
+            project['progress'] = project['current'] / project['goal'];
+            result.status(200).send();
         } else {
             result.sendStatus(400);
         }
@@ -75,20 +115,34 @@ app.get('/api/get_project', function (request, result) {
     });
 });
 
-function donate(amount, projectId, result) {
+function donate(amount, projectId, user, result) {
     databaseRef.child(`projects/${projectId}`).once('value').then(function(snapshot) {
         if (snapshot.val()) {
             const current = snapshot.val()['current'];
             const goal = snapshot.val()['goal'];
             const amtRemaining = goal - current;
             const donationAmt = (amtRemaining < amount) ? amtRemaining : amount;
+
             databaseRef.child(`projects/${projectId}/current`).set(donationAmt + current);
-            if (result) {
-                result.status(200).send({
-                    "current": (donationAmt + current),
-                    "goal": goal
-                });
-            }
+            databaseRef.child(`projects/${projectId}/pledges/${user}`)
+            .once('value')
+            .then(function(snapshot) {
+                var amountPledged = 0;
+                if (snapshot.val()) {
+                    amountPledged = snapshot.val()['amount'];
+                }
+                
+                databaseRef.child(`projects/${projectId}/pledges/${user}/amount`)
+                .set(amountPledged + amount);
+
+                if (result) {
+                    result.status(200).send({
+                        "current": (donationAmt + current),
+                        "goal": goal,
+                        "total_pledged": (amount + amountPledged)
+                    });
+                }
+            });
         } else {
             if (result) {
                 result.sendStatus(400);
